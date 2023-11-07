a7a3cbc984c636377e7c76d47b1c6b63c40d2d9c
Tomek <tomek.bielicki@gmail.com>
2023-11-07 18:40:43 +0100

    episods list section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { Store } from "./Store";
 
+interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: null;
+  name: string;
+  number: number;
+  rating: { average: null };
+  runtime: number;
+  season: number;
+  summary: string;
+  type: string;
+  url: string;
+  _links: { self: {}; show: {} };
+}
+
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store);
 
@@ -25,6 +42,19 @@ export default function App(): JSX.Element {
     <React.Fragment>
       <h1>Rick and Morty</h1>
       <p>Pick your favourit episod</p>
+      <section>
+        {state.episodes.map((episode: IEpisode) => {
+          console.log(episode);
+          return (
+            <section key={episode.id}>
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season} Number: {episode.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </React.Fragment>
   );
 }
