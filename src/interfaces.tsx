5c270972314bddcda46b5541d04c718b3650f0db
Tomek <tomek.bielicki@gmail.com>
2023-11-07 18:52:48 +0100

    move interfeces to one file

---
+++ src/interfaces.tsx
@@ -0,0 +1,25 @@
+export interface IState {
+  episodes: Array<IEpisode>;
+  favourite: Array<any>;
+}
+export interface IAction {
+  type: string;
+  payload: any;
+}
+
+export interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: null;
+  name: string;
+  number: number;
+  rating: { average: null };
+  runtime: number;
+  season: number;
+  summary: string;
+  type: string;
+  url: string;
+  _links: { self: {}; show: {} };
+}
